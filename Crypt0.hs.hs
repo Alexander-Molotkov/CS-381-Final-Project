a83c8c5baa04d360e052ec8800a62addf05c20db
Alexander Molotkov <molotkoa@oregonstate.edu>
2020-02-23 13:23:26 -0800

    Renamed project, added TODO's that we talked about at our meeting.

--- Crypt0.hs.hs
+++ Crypt0.hs.hs
@@ -1,8 +1,14 @@
-module Untitled where
+module Crypt0 where
 
 -- State of program is the value of all of the variables
 -- Semantic domain (?): [Var] -> [Var] = State -> State
 -- Stretch goal: pointers - reference variable by location in State
+
+-- TODO (Feb 27th)
+--    - CMDs: Sub, Call, If, While/For
+--    - VARs: Int, Bool, String, (Maybe float)
+--    - Features: Start on static typing, 
+
 type State = [Var] 
 type Prog  = [Cmd]
 type Name  = String
@@ -34,38 +40,37 @@ run [] s = s
 
 cmd :: Cmd -> State -> State
 cmd c s = case c of
-    Declare ref v   -> pushVar ref v s
+    Declare ref v   -> set ref v s
     Add r v1 v2 -> add r v1 v2 s
     -- Other commands
 
 -- Goes through stack and gets specific variable's value by reference
-pullVar :: Name -> State -> Var 
-pullVar ref (v:vs) = case v of
-   Int  name val -> if name == ref then v else pullVar ref vs
-   Bool name val -> if name == ref then v else pullVar ref vs
+get :: Name -> State -> Var 
+get ref (v:vs) = case v of
+   Int  name val -> if name == ref then v else get ref vs
+   Bool name val -> if name == ref then v else get ref vs
 -- TODO: Variable does not exist case
-pullVar _ [] = undefined
+get _ [] = undefined
 
 -- Changes the value of a variable on the stack
 -- Right now you can change types into other types dynamically - Bools can become Ints, etc, this feels bad.
 -- Names can also change - double bad?
 -- Have to think about what restrictions we want on variable manipulation - do we want variables to-
    -- be autodeclared like python if they don't already exist? Or more C-like strict typing?
--- TODO: Variables should overwite themselves not declare a new variable
-pushVar :: Name -> Var -> State -> State
-pushVar ref i s = (declareVar ref i (removeVar i s))
+set :: Name -> Var -> State -> State
+set ref i s = (declareVar ref i (removeVar i s))
 
 declareVar :: Name -> Var -> State -> State
 declareVar ref i (v:vs) = 
   case v of
     Int name val -> 
         if name == ref 
-        then [i] ++ pushVar ref i (removeVar i vs)
-        else  v   : pushVar ref i (removeVar i vs)
+        then [i] ++ set ref i (removeVar i vs)
+        else  v   : set ref i (removeVar i vs)
     Bool name val -> 
         if name == ref 
-        then [i] ++ pushVar ref i (removeVar i vs)
-        else  v   : pushVar ref i (removeVar i vs)               
+        then [i] ++ set ref i (removeVar i vs)
+        else  v   : set ref i (removeVar i vs)               
 declareVar ref i [] = [i]
 
 -- Removes a variable from scope
@@ -78,11 +83,11 @@ removeVar i [] = []
 
 -- Adds together two variables
 add :: Name -> Name -> Name -> State -> State
-add result a1 a2 s = pushVar result (Int result (x+y)) s
+add result a1 a2 s = declareVar result (Int result (x+y)) s
     where 
-    x = case pullVar a1 s of
+    x = case get a1 s of
         (Int name v) -> v    
-    y = case pullVar a2 s of
+    y = case get a2 s of
         (Int name v) -> v
 
 -- Testing 
@@ -92,8 +97,3 @@ s0 = []
 
 -- run prog s0
 prog = [Declare "num1" (Int "num1" 1), Declare "num2" (Int "num2" 3), Add "sum" "num1" "num2"]
-
-
-
-
-
