18e747b41d4968ddb3b534c43f9311ae929703fc
Alexander Molotkov <molotkoa@oregonstate.edu>
2020-02-25 17:02:02 -0800

    Finished up arithmetic operations and cemented variable types Added if statements Added 'op' data type for use with arithmetic operations Added doctests for arithmetic operations Next step is functions

--- Crypt0.hs
+++ Crypt0.hs
@@ -10,13 +10,17 @@ import Data.Map
 --    - VARs: Int, Bool, String, (Maybe float)
 --    - Features: Start on static typing, 
 
+-- TYPE DECLARATIONS
+
 type State = Map Name Var
 type Prog  = [Cmd]
 type Name  = String
 
 data Cmd = Declare Name Var
          | Add Name Name Name
-      --  Sub Name Name
+         | Sub Name Name Name
+         | Mul Name Name Name
+         | Div Name Name Name
          | If Name Cmd Cmd
       --  Call Function [Var] OR Call Function [Name] (value or reference?)
       --  TODO: More cmds
@@ -27,7 +31,11 @@ data Var = Int Int
          | Double Double
          | Bool Bool
          | String String
+      --  Function Prog [Name]
       --  TODO: More types
+    deriving (Eq,Show)
+
+data Op = Plus | Minus | Mult | Divi
     deriving (Eq,Show)
 
 -- TODO: Function = String (name of function) -> [String OR Var] (variables) -> Prog (code) -> State -> State
@@ -35,25 +43,48 @@ data Var = Int Int
 
 -- Stretch goal: classes
 
+-- SEMANTICS
+
 run :: Prog -> State -> State
 run (c:cs) s = run cs (cmd c s)
 run [] s = s 
 
 cmd :: Cmd -> State -> State
 cmd c s = case c of
+    -- Variable declaration
     Declare ref v -> set ref v s
+    -- Addition
     Add r v1 v2   -> 
-        if (typeOf v1 s) == (typeOf v2 s)
-        then case typeOf v1 s of 
-            "Int"  -> addNum r v1 v2 s 
-            "Dbl"  -> addNum r v1 v2 s
-            "Str"  -> addStr r v1 v2 s
+      case typeOf v1 s of
+        "Int"     -> set r (Int (performOpInt (valInt v1 s) (valInt v2 s) Plus)) s
+        "Dbl"     -> set r (Double (performOpDbl (valDbl v1 s) (valDbl v2 s) Plus)) s
+        "Str"     -> set r (String (performOpStr (valStr v1 s) (valStr v2 s) Plus)) s
+        otherwise -> error ("Invalid variable type passed to 'Add': " ++ typeOf v1 s) 
+    -- Subtraction
+    Sub r v1 v2   ->
+      case typeOf v1 s of
+        "Int"     -> set r (Int (performOpInt (valInt v1 s) (valInt v2 s) Minus)) s
+        "Dbl"     -> set r (Double (performOpDbl (valDbl v1 s) (valDbl v2 s) Minus)) s
+        -- TODO: "Str"     -> set r (String (performOpStr (valStr v1 s) (valStr v2 s) Minus)) s
+        otherwise -> error ("Invalid variable type passed to 'Sub': " ++ typeOf v1 s) 
+    -- Multiplication
+    Mul r v1 v2   ->
+      case typeOf v1 s of
+        "Int"     -> set r (Int (performOpInt (valInt v1 s) (valInt v2 s) Mult)) s
+        "Dbl"     -> set r (Double (performOpDbl (valDbl v1 s) (valDbl v2 s) Mult)) s
+        otherwise -> error ("Invalid variable type passed to 'Add': " ++ typeOf v1 s)    
+    -- Division
+    Div r v1 v2   ->
+      case typeOf v1 s of
+        "Int"     -> set r (Int (performOpInt (valInt v1 s) (valInt v2 s) Divi)) s
+        "Dbl"     -> set r (Double (performOpDbl (valDbl v1 s) (valDbl v2 s) Divi)) s
+        otherwise -> error ("Invalid variable type passed to 'Add': " ++ typeOf v1 s)    
+    -- If statement
     If b c1 c2    -> 
         if (case get b s of
               Bool v -> v)
         then cmd c1 s
         else cmd c2 s
-                   
     -- Other commands
 
 -- | Goes through stack and gets specific variable's value by reference
@@ -62,6 +93,52 @@ cmd c s = case c of
 -- >>> get "var" s1
 -- Int 3
 --
+
+--
+-- MATH OPERATIONS
+--
+
+performOpInt :: Int -> Int -> Op -> Int
+performOpInt x y o = 
+    case o of
+      Plus  -> x + y
+      Minus -> x - y
+      Mult  -> x * y
+      Divi  -> x `div` y
+
+valInt :: Name -> State -> Int
+valInt v s = 
+    case get v s of
+      Int x -> x
+
+performOpDbl :: Double -> Double -> Op -> Double
+performOpDbl x y o = 
+    case o of
+      Plus  -> x + y
+      Minus -> x - y
+      Mult  -> x * y
+      Divi  -> x / y
+
+valDbl :: Name -> State -> Double
+valDbl v s = 
+    case get v s of
+      Double x -> x
+
+performOpStr :: String -> String -> Op -> String
+performOpStr x y o = 
+    case o of
+      Plus  -> x ++ y
+      --TODO: Minus -> x - y
+
+valStr :: Name -> State -> String
+valStr v s = 
+    case get v s of
+      String x -> x
+
+--
+-- VARIABLE MANIPULATION
+--
+
 get :: Name -> State -> Var 
 get key s = s ! key
 -- TODO: Variable does not exist case
@@ -84,56 +161,68 @@ typeOf key s = case get key s of
 removeVar :: Name -> State -> State
 removeVar key s = delete key s
 
--- | Adds together two numbers
+-- TESTING
+
+-- | Adds together two vars
 --
 -- >>> prog = [Declare "num1" (Int 5), Declare "num2" (Int 15), Add "sum" "num1" "num2"]
 -- >>> s1 = run prog s0 
 -- >>> get "sum" s1
 -- Int 20
 --
--- >>> prog = [Declare "num1" (Double 8.2), Declare "num2" (Double 3.6), Add "sum" "num1" "num2"]
+-- >>> prog = [Declare "num1" (Double 8.20), Declare "num2" (Double 3.60), Add "sum" "num1" "num2"]
 -- >>> s1 = run prog s0
 -- >>> get "sum" s1
--- Double 11.8
+-- Double 11.799999999999999
 --
-addNum :: Name -> Name -> Name -> State -> State
-addNum result a1 a2 s = set result (Int (x + y)) s
-    where
-      x = case get a1 s of
-        Int v    -> v
-      y = case get a2 s of
-        Int v    -> v
-
--- | Adds together two strings
--- 
+-- NOTE: We may want to take a look at why double addition works like this,
+-- could be something that we just have to deal with.
+--
 -- >>> prog = [Declare "str1" (String "asd"), Declare "str2" (String "123"), Add "str3" "str1" "str2"]
 -- >>> s1 = run prog s0
 -- >>> get "str3" s1
 -- String "asd123"
 --
-addStr :: Name -> Name -> Name -> State -> State
-addStr result a1 a2 s = set result (String (x ++ y)) s
-    where 
-      x = case get a1 s of
-        String s -> s
-      y = case get a2 s of
-        String s -> s
+-- | Subtracts two vars
+--
+-- >>> prog = [Declare "num1" (Int 5), Declare "num2" (Int 15), Sub "sum" "num1" "num2"]
+-- >>> s1 = run prog s0 
+-- >>> get "sum" s1
+-- Int (-10)
+--
+-- >>> prog = [Declare "num1" (Double 8.20), Declare "num2" (Double 3.60), Sub "sum" "num1" "num2"]
+-- >>> s1 = run prog s0
+-- >>> get "sum" s1
+-- Double 4.6
+--
+-- | Multiplies two vars
+--
+-- >>> prog = [Declare "num1" (Int 2), Declare "num2" (Int 15), Mul "sum" "num1" "num2"]
+-- >>> s1 = run prog s0 
+-- >>> get "sum" s1
+-- Int 30
+--
+-- >>> prog = [Declare "num1" (Double 8.20), Declare "num2" (Double 2.0), Mul "sum" "num1" "num2"]
+-- >>> s1 = run prog s0
+-- >>> get "sum" s1
+-- Double 16.4
+--
+-- | Divides two vars
+--
+-- >>> prog = [Declare "num1" (Int 30), Declare "num2" (Int 15), Div "sum" "num1" "num2"]
+-- >>> s1 = run prog s0 
+-- >>> get "sum" s1
+-- Int 2
+--
+-- >>> prog = [Declare "num1" (Double 8.20), Declare "num2" (Double 2.0), Div "sum" "num1" "num2"]
+-- >>> s1 = run prog s0
+-- >>> get "sum" s1
+-- Double 4.1
+--
 
-
---TODO:
-
---addNum' :: Name -> Name -> Name -> State -> State
---addNum' = add Int (+)
-
---add :: (a -> Var) -> (b -> b -> b) -> (Name -> Name -> Name -> State -> State)
---add type_ op = set (type_ (f `op` f))
-    
-
--- Testing 
--- TODO: Doctests
 s0 :: State
 s0 = empty
 
 --run prog s0
-prog = [Declare "num1" (Double 8.2), Declare "num2" (Double 3.6), Add "sum" "num1" "num2"]
-prog1 = [Declare "bool1" (Bool True), If "bool1" (Declare "true" (Int 1)) (Declare "false" (Int 0)]
+prog = [Declare "num1" (Double 8.2), Declare "num2" (Double 3.3), Add "sum" "num1" "num2"]
+prog1 = [Declare "bool1" (Bool True), If "bool1" (Declare "true" (Int 1)) (Declare "false" (Int 0))]
