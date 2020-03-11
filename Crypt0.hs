77d3508f72832b90fcda7ee6c8cfc932fc840737
Alexander Molotkov <molotkoa@oregonstate.edu>
2020-03-11 11:35:23 -0700

    Changed data type names to be more descriptive
    Created a function that returns the type of a variable to support type
     checking

--- Crypt0.hs
+++ Crypt0.hs
@@ -36,12 +36,12 @@ data Expr = BinOp Op Expr Expr
           | Call Name [Expr]
     deriving (Eq,Show)
 
-data Var = I Int
-         | D Double
-         | B Bool
-         | S String
+data Var = Int Int
+         | Dbl Double
+         | Bul Bool
+         | Str String
          -- Function
-         | F Type [(Type, Name)] Prog
+         | Fun Type [(Type, Name)] Prog
     deriving (Eq,Show)
 
 -- The arithmetic operators that we support
@@ -71,7 +71,7 @@ cmd command s = case command of
     Declare ref e   -> set ref (expr e s) s
     -- If statement
     If e c1 c2      -> case expr e s of
-        (B b)           -> if b then driver c1 s else driver c2 s
+        (Bul b)           -> if b then driver c1 s else driver c2 s
     -- While loop 
     While e c       -> while e c s
 
@@ -82,18 +82,18 @@ expr e s = case e of
         (v1, v2) -> op o v1 v2
     -- Less than check
     Lt e1 e2  -> case (expr e1 s, expr e2 s) of
-        (I v1, I v2) -> B (v1 < v2)
-        (D v1, D v2) -> B (v1 < v2)
+        (Int v1, Int v2) -> Bul (v1 < v2)
+        (Dbl v1, Dbl v2) -> Bul (v1 < v2)
     -- Greater than check
     Gt e1 e2  -> case (expr e1 s, expr e2 s) of
-        (I v1, I v2) -> B (v1 > v2)
-        (D v1, D v2) -> B (v1 > v2)
+        (Int v1, Int v2) -> Bul (v1 > v2)
+        (Dbl v1, Dbl v2) -> Bul (v1 > v2)
     -- Equality check
     Eq e1 e2  -> case (expr e1 s, expr e2 s) of
-        (I v1, I v2) -> B (v1 == v2)
-        (D v1, D v2) -> B (v1 == v2)
-        (B v1, B v2) -> B (v1 == v2)
-        (S v1, S v2) -> B (v1 == v2)
+        (Int v1, Int v2) -> Bul (v1 == v2)
+        (Dbl v1, Dbl v2) -> Bul (v1 == v2)
+        (Bul v1, Bul v2) -> Bul (v1 == v2)
+        (Str v1, Str v2) -> Bul (v1 == v2)
     -- Call function
     Call ref es -> call ref es s
     -- Lit
@@ -106,9 +106,10 @@ expr e s = case e of
 --
 
 -- Static type checking for each prog
-typeCheck :: Prog -> [(Name, Type)] -> maybe String
 -- QUESTION: Check repeated declares?
-typeCheck (c:cs) = case c of
+typeCheck :: Prog -> [(Name, Type)] -> Maybe String
+typeCheck [] s = Nothing
+typeCheck (c:cs) s = case c of
     (Declare ref e) -> undefined
     (If e p1 p2)    -> undefined
     (While e p)     -> undefined
@@ -124,27 +125,35 @@ typeExpr (Eq e1 e2)        s = undefined
 typeExpr (BinOp op e1 e2)  s = undefined
 typeExpr (Call ref (e:es)) s = undefined
 
+-- Gets type of a variable
+typeOf :: Var -> Type
+typeOf (Int _)     = Int_ty
+typeOf (Dbl _)     = Dbl_ty
+typeOf (Bul _)     = Bul_ty
+typeOf (Str _)     = Str_ty
+typeOf (Fun _ _ _) = Fun_ty
+
 --
 -- BINARY OPERATORS
 --
 
 op :: Op -> Var -> Var -> Var
 -- Addition
-op Add (I s1) (I s2) = I (s1 + s2)
-op Add (D s1) (D s2) = D (s1 + s2)
-op Add (S s1) (S s2) = S (s1 ++ s2)
+op Add (Int s1) (Int s2) = Int (s1 + s2)
+op Add (Dbl s1) (Dbl s2) = Dbl (s1 + s2)
+op Add (Str s1) (Str s2) = Str (s1 ++ s2)
 -- Subtraction
-op Sub (I s1) (I s2) = I (s1 - s2)
-op Sub (D s1) (D s2) = D (s1 - s2)
+op Sub (Int s1) (Int s2) = Int (s1 - s2)
+op Sub (Dbl s1) (Dbl s2) = Dbl (s1 - s2)
 -- TODO: op Sub (String s1) (String s2) = String (s1 -- s2)
 -- Multiplication
-op Mul (I s1) (I s2) = I (s1 * s2)
-op Mul (D s1) (D s2) = D (s1 * s2)
+op Mul (Int s1) (Int s2) = Int (s1 * s2)
+op Mul (Dbl s1) (Dbl s2) = Dbl (s1 * s2)
 -- Division
-op Div (I s1) (I s2) = if s2 /= 0
-    then I (s1 `div` s2) else error "ERROR: attempt to divide by 0"
-op Div (D s1) (D s2) = if s2 /= 0 
-    then D (s1 / s2) else error "ERROR: attempt to divide by 0"
+op Div (Int s1) (Int s2) = if s2 /= 0
+    then Int (s1 `div` s2) else error "ERROR: attempt to divide by 0"
+op Div (Dbl s1) (Dbl s2) = if s2 /= 0 
+    then Dbl (s1 / s2) else error "ERROR: attempt to divide by 0"
 --
 -- VARIABLE MANIPULATION
 --
@@ -166,12 +175,12 @@ removeVar key s = delete key s
 -- Increment and Decrement
 iter ::  Name -> Op -> State -> Prog
 iter ref o s = case get ref s of 
-    (I i) -> [Declare ref (BinOp o (Lit (I i)) (Lit (I 1)))]
-    (D d) -> [Declare ref (BinOp o (Lit (D d)) (Lit (D 1)))]
+    (Int i) -> [Declare ref (BinOp o (Lit (Int i)) (Lit (Int 1)))]
+    (Dbl d) -> [Declare ref (BinOp o (Lit (Dbl d)) (Lit (Dbl 1)))]
 
 -- Pulls the value out of a bool variable
 valBool :: Var -> Bool
-valBool (B b) = b
+valBool (Bul b) = b
 
 varExists :: Name -> State -> Bool
 varExists ref s = case Data.Map.lookup ref s of
@@ -199,7 +208,7 @@ while e p s = if valBool (expr e s) then while e p (driver p s) else s
 -- Our functions have static scoping and are pass by value
 call :: Name -> Params -> State -> Var
 call ref prms s = case get ref s of
-    (F _ fVars body) ->
+    (Fun _ fVars body) ->
         let s' = getParams fVars prms s
         in doFunc body s' 
 
@@ -245,65 +254,65 @@ s0 = empty
 --
 
 --run <program> s0
-addProg = run [Declare "num" (Lit (I 23)), 
-               Declare "num2" (Lit (I 24)), 
+addProg = run [Declare "num" (Lit (Int 23)), 
+               Declare "num2" (Lit (Int 24)), 
                Declare "Result" (BinOp Add (Get "num") (Get "num2"))] s0
          
-subProg = run [Declare "num" (Lit (I 23)), 
-               Declare "num2" (Lit (I 24)), 
+subProg = run [Declare "num" (Lit (Int 23)), 
+               Declare "num2" (Lit (Int 24)), 
                Declare "Result" (BinOp Sub (Get "num") (Get "num2"))] s0        
            
-mulProg = run [Declare "num" (Lit (I 23)), 
-               Declare "num2" (Lit (I 24)), 
+mulProg = run [Declare "num" (Lit (Int 23)), 
+               Declare "num2" (Lit (Int 24)), 
                Declare "Result" (BinOp Mul (Get "num") (Get "num2"))] s0      
 
-divProg = run [Declare "num" (Lit (I 23)), 
-               Declare "num2" (Lit (I 24)), 
+divProg = run [Declare "num" (Lit (Int 23)), 
+               Declare "num2" (Lit (Int 24)), 
                Declare "Result" (BinOp Div (Get "num") (Get "num2"))] s0        
 
-divZeroProg = run [Declare "num" (Lit (I 23)), 
-                   Declare "num2" (Lit (I 0)), 
+divZeroProg = run [Declare "num" (Lit (Int 23)), 
+                   Declare "num2" (Lit (Int 0)), 
                    Declare "Result" (BinOp Div (Get "num") (Get "num2"))] s0        
 
-ifProg = run [Declare "true" (Lit (B True)), 
+ifProg = run [Declare "true" (Lit (Bul True)), 
               If (Get "true") 
-                  [(Declare "True" (Lit (I 1)))]
-                  [(Declare "False" (Lit (I 0)))]] s0
+                  [(Declare "True" (Lit (Int 1)))]
+                  [(Declare "False" (Lit (Int 0)))]] s0
 
-ltProg = run [Declare "lt" (Lit (I 5)), 
-              Declare "gt" (Lit (I 10)),
+ltProg = run [Declare "lt" (Lit (Int 5)), 
+              Declare "gt" (Lit (Int 10)),
               If (Lt (Get "lt") (Get "gt"))
-                 [Declare "True" (Lit (B True))] 
-                 [Declare "False" (Lit (B False))]] s0
+                 [Declare "True" (Lit (Bul True))] 
+                 [Declare "False" (Lit (Bul False))]] s0
 
-gtProg = run [Declare "lt" (Lit (I 5)), 
-              Declare "gt" (Lit (I 10)), 
+gtProg = run [Declare "lt" (Lit (Int 5)), 
+              Declare "gt" (Lit (Int 10)), 
               If (Gt (Get "gt") (Get "lt"))
-                 [Declare "True" (Lit (B True))] 
-                 [Declare "False" (Lit (B False))]] s0
+                 [Declare "True" (Lit (Bul True))] 
+                 [Declare "False" (Lit (Bul False))]] s0
 
-eqProg = run [Declare "1" (Lit (I 10)), 
-              Declare "2" (Lit (I 10)), 
+eqProg = run [Declare "1" (Lit (Int 10)), 
+              Declare "2" (Lit (Int 10)), 
               If (Eq (Get "1") (Get "2"))
-                 [Declare "True" (Lit (B True))] 
-                 [Declare "False" (Lit (B False))]] s0
+                 [Declare "True" (Lit (Bul True))] 
+                 [Declare "False" (Lit (Bul False))]] s0
 
 nullVarProg = run [Declare "i" (Get "null")] s0
 
---iterProg = run [Declare "i" (Lit (D 0)),
+--iterProg = run [Declare "i" (Lit (Dbl 0)),
 --             Iterate Inc "i",
 --             Iterate Inc "i", 
 --             Iterate Dec "i"] s0
 
---whileProg = run [Declare "i" (Lit (I 0)),
---                 While (Lt (Get "i") (Lit (I 5))) 
+--whileProg = run [Declare "i" (Lit (Int 0)),
+--                 While (Lt (Get "i") (Lit (Int 5))) 
                      --[Iterate Inc "i"]] s0
 
---forProg = run [Declare "y" (Lit (I 10)),
---               For ("i", Lit (I 0)) (Lt (Get "i") (Lit (I 7))) Inc
+--forProg = run [Declare "y" (Lit (Int 10)),
+--               For ("i", Lit (Int 0)) (Lt (Get "i") (Lit (Int 7))) Inc
                    --[Iterate Dec "y"]] s0
 
 -- Function Type [(Type, Name)] Prog
---funProg = run [Declare "fun" (Lit (F Int_ty [(Int_ty, "x")]
---                   [Return (BinOp Add (Get "x") (Lit (I 3)))])),
---               Declare "result" (Call "fun" [(Lit (I 5))])] s0
+--funProg = run [Declare "fun" (Lit (Fun Int_ty [(Int_ty, "x")]
+--                   [Return (BinOp Add (Get "x") (Lit (Int 3)))])),
+--               Declare "result" (Call "fun" [(Lit (Int 5))])] s0
